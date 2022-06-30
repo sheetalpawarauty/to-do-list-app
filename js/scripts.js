28334e6b5e47ba26c888a6a46e952b446e6a1a02
SheetalPawarAuty <102964955+SheetalPawarAuty@users.noreply.github.com>
2022-06-30 10:48:30 +0200

    Update scripts.js
    If applied, this commit will have the 'X' button visible.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,35 @@
 // jQuery Code
 //1. Adding a new item to the list:
 function newItem(){
-let li = $('<li></li>');
-let inputValue = $('#input').val();
-li.append(inputValue);
+  
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
 
-if (inputValue === '') {
-  alert("You must write something!");
-} else {
-  $('#list').append(li);
-}
-}
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    $('#list').append(li);
+  }
 
 //2. Crossing an item out:
-function crossOut() {
-  li.toggleClass("strike");
-}
+  function crossOut() {
+    li.toggleClass("strike");
+  }
 
-li.on("dblclick", function crossOut() {
-  li.toggleClass("strike");
-});
+  li.on("dblclick", function crossOut() {
+    li.toggleClass("strike");
+  });
 
 //3. Adding a delete button
-let crossOutButton = $('<crossOutButton></crossOutButton>');
-crossOutButton.append(document.createTextNode('X'));
-li.append(crossOutButton);
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
 
-crossOutButton.on("click", deleteListItem);
-function deleteListItem(){
-li.addClass("delete")
+  crossOutButton.on("click", deleteListItem);
+    function deleteListItem(){
+      li.addClass("delete")
+  }
+
+  $('#list').sortable();
 }
-
-$('#list').sortable();
